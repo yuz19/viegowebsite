619ec6bcde9a0c94f04113b7dcad206fdb7c8e0f
nabil19 <funity111@gmail.com>
2023-05-31 15:54:11 +0100

    first commit

---
+++ src/components/Acceuil.js
@@ -0,0 +1,14 @@
+import React from 'react'
+function Acceuil(){
+    return(
+    <div className='acceuil'>
+        <h1 id='titleB'>
+        <span>Ruined</span> <span>King</span>
+        </h1>
+
+       
+        <img src="./sword.png" alt='' height={500}/>
+    </div>
+    )
+}
+export default Acceuil
---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+function NavBar(){
+    return(
+        <header>
+        <h1>Viego</h1>
+        <nav>
+            <ul>
+                <li>Story</li>
+                <li>Build</li>
+                <li>Skins</li>
+                <li>Update Informations</li>
+
+            </ul>
+        </nav>
+        </header>
+
+    )
+}
+export default NavBar
